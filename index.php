ceb71fdb6278a20fe2f348ca65cb0468d37db5f8
Jorlee23 <56898668+Jorlee23@users.noreply.github.com>
2021-04-28 18:15:27 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -15,7 +15,34 @@ Game Genre: <input type="text" name="g_genre" /><br><br>
 
 <input type="submit" value="Insert">
 
+<?php
 
+$username="dbmasteruser";
+$password="2!Ib+WXS7k6+d~`t_YA8CZsy,k=w>F!G";
+$hostname="ls-5043d85224f7bec8c5e6623252b03f44b243b984.cnlujzbcphmv.us-west-2.rds.amazonaws.com";
+$database="bitnami_wordpress";
+
+$con = mysqli_connect($hostname, $username, $password)
+  or die("Connection Failed");
+  
+
+
+$dbconn = mysqli_select_db($con, $database)
+  or die("DB not selected");
+
+	$sql2 = "select * from Game";
+	$result = $con->query($sql2);
+	
+	if($result->num_rows > 0){
+		while($row = $result->fetch_assoc() ) {
+			echo $row["g_name"] . "  " .$row["g_price"]. "<br><br>";
+		}
+	}
+	else {
+		echo "0 records";
+	}
+
+?>
 
 </select>
 
